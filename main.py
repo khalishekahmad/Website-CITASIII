342a4b1be9629845447ad5a87e387fb51a1343c9
Murdangs <ardhien50@gmail.com>
2024-05-12 15:46:37 +0700

    Rapihin tampilan website

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
-from asyncio import run
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-import account,kalkulator,metode,penjelasan
+import account, kalkulator, metode, penjelasan
+
 st.set_page_config(
-        page_title="Citasi",
-        page_icon="https://github.com/ardhien50/Website-Citasi_coba/blob/main/WebsiteCitasi/Gambar/Logo%20Web%20Citasi.png?raw=true",  # Ganti dengan URL logo Anda
-        layout="centered"  # Layout halaman
+    page_title="Citasi",
+    page_icon="https://github.com/ardhien50/Website-Citasi_coba/blob/main/WebsiteCitasi/Gambar/Logo%20Web%20Citasi.png?raw=true",  # Ganti dengan URL logo Anda
+    layout="centered"  # Layout halaman
 )
 
 class MultiApp:
     def __init__(self):
         self.apps = []
 
-    def add_app(self,title, func):
+    def add_app(self, title, func):
         self.apps.append({
             "title": title,
             "function": func
         })
 
+    @staticmethod
     def run():
         with st.sidebar:        
             app = option_menu(
-                menu_title='Citasi ',
-                options=['Account','Kalkulator','Metode','Penjelasan'],
-                icons=['person-circle','chat-text-fill','house-fill','info-circle-fill'],
-                menu_icon='chat-text-fill',
+                menu_title='Citasi',
+                options=['Account', 'Kalkulator', 'Metode', 'Penjelasan'],
+                icons=['person-circle', 'calculator', 'globe', 'info-circle'],
+                menu_icon='water',  # Ubah dengan ikon yang sesuai dari FontAwesome
                 default_index=1,
                 styles={
-                    "container": {"padding": "5!important","background-color":'grey'},
-        "icon": {"color": "white", "font-size": "23px"}, 
-        "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "black"},
-        "nav-link-selected": {"background-color": "black"},}
-                
-                )
+                    "container": {"padding": "5!important", "background-color": 'grey'},
+                    "icon": {"color": "white", "font-size": "23px"}, 
+                    "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "0px", "--hover-color": "black"},
+                    "nav-link-selected": {"background-color": "black"}
+                }
+            )
 
         if app == "Account":
             account.app()
@@ -44,4 +45,14 @@ class MultiApp:
         if app == "Penjelasan":
             penjelasan.app()
 
-    run()
+# Instansiasi MultiApp
+multi_app = MultiApp()
+
+# Tambahkan aplikasi ke MultiApp
+multi_app.add_app("Account", account.app)
+multi_app.add_app("Kalkulator", kalkulator.app)
+multi_app.add_app("Metode", metode.app)
+multi_app.add_app("Penjelasan", penjelasan.app)
+
+# Jalankan aplikasi
+multi_app.run()
--- penjelasan.py
+++ penjelasan.py
@@ -77,44 +77,78 @@ def app():
     except Exception as e:
         st.error(f"Error fetching and displaying image: {e}")
 
+    st.subheader('Dataset')
+    with st.container():
+        col1, col2, col3 = st.columns(3)
+
+    with col1:
+        image_div = st.empty()
+        st.write("")
+        st.image('https://github.com/ardhien50/Website-Citasi_coba/blob/main/WebsiteCitasi/Gambar/images.png?raw=true')
+
+    with col2:
+        image_div = st.empty()
+        st.write("")
+        st.image('https://github.com/ardhien50/Website-Citasi_coba/blob/main/WebsiteCitasi/Gambar/7b4924a3f4fa7fffbe7e132073440d18.png?raw=true')
+
+    with col3:
+        image_div = st.empty()
+        st.write("")
+        st.image('https://github.com/ardhien50/Website-Citasi_coba/blob/main/WebsiteCitasi/Gambar/images%20(1).png?raw=true')
+    
+    st.write('Dengan menggunakan dataset yang sudah didapatkan dari Dinas Lingkungan Hidup dan Kebersihan Kota Bandung, nantinya akan dipilih tiga metode pembelajaran mesin yang dikiranya dapat memberikan hasil yang optimal. Di dalam dataset tersebut terdiri dari parameter kualitas air yaitu pH, TSS, DO, BOD, COD, Nitrat, Fecal Coliform, Fosfat, dan IP. Kemudian memisahkan datanya menjadi data latih dan data uji lalu membuat model pembelajaran mesin dari setiap model yang sudah ditentukan sebagai alternatif usulan solusi untuk klasifikasi kualitas air Sungai Citarum.')
+    st.link_button('Tempat Mendapatkan Dataset', 'https://opendata.jabarprov.go.id/id')
+    
     st.subheader("Kandungan Dalam Air")
     st.write('Berikut merupakan penjelasan mengenai data kandungan air yang terdapat pada website Citasi. Website akan menampilkan data data yang lengkap pada setiap titik poin. Dimana data yang yang diberikan akan berisikan, pH, TSS, DO, BOD, COD, Nitrat, Fecal Coliform, Fosfat, dan IP yang nantinya akan digunakan sebagai indikator untuk kualitas air pada titik tersebut.')
 
-    st.subheader('1. pH')
-    st.write('pH (Potential of Hydrogen) adalah derajat keasaman yang digunakan untuk menyatakan tingkat keasaman atau kebasaan yang dimiliki oleh suatu larutan. Ia didefinisikan sebagai kologaritma aktivitas ion hidrogen (H+) yang terlarut. Koefisien aktivitas ion hidrogen tidak dapat diukur secara eksperimental, sehingga nilainya didasarkan pada perhitungan teoretis. Skala pH bukanlah skala absolut. Ia bersifat relatif terhadap sekumpulan larutan standar yang pH-nya ditentukan berdasarkan persetujuan internasional.')
-    st.link_button("Bca Selengkapnya", "https://www.usgs.gov/special-topics/water-science-school/science/ph-and-water#:~:text=oil%20or%20alcohol.-,pH%20is%20a%20measure%20of%20how%20acidic%2Fbasic%20water%20is,hydroxyl%20ions%20in%20the%20water.")
+    with st.container():
+        col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        st.subheader('1. pH')
+        st.write('pH (Potential of Hydrogen) adalah derajat keasaman yang digunakan untuk menyatakan tingkat keasaman atau kebasaan yang dimiliki oleh suatu larutan. Ia didefinisikan sebagai kologaritma aktivitas ion hidrogen (H+) yang terlarut. Koefisien aktivitas ion hidrogen tidak dapat diukur secara eksperimental, sehingga nilainya didasarkan pada perhitungan teoretis. Skala pH bukanlah skala absolut. Ia bersifat relatif terhadap sekumpulan larutan standar yang pH-nya ditentukan berdasarkan persetujuan internasional.')
+        st.link_button("Bca Selengkapnya", "https://www.usgs.gov/special-topics/water-science-school/science/ph-and-water#:~:text=oil%20or%20alcohol.-,pH%20is%20a%20measure%20of%20how%20acidic%2Fbasic%20water%20is,hydroxyl%20ions%20in%20the%20water.")
 
-    st.subheader('2. TSS')
-    st.write('TSS (Total Suspended Solid) atau total padatan tersuspensi adalah padatan yang tersuspensi di dalam air berupa bahan-bahan organik dan inorganic yang dapat disaring dengan kertas millipore berpori pori 0,45 μm. Materi yang tersuspensi mempunyai dampak buruk terhadap kualitas air karena mengurangi penetrasi matahari ke dalam badan air, kekeruhan air meningkat yang menyebabkan gangguan pertumbuhan bagi organisme produser.')
-    st.link_button('Baca Selengkapnya', 'https://www.handalselaras.com/total-suspended-solid-tss/')
+    with col2:
+        st.subheader('2. TSS')
+        st.write('TSS (Total Suspended Solid) atau total padatan tersuspensi adalah padatan yang tersuspensi di dalam air berupa bahan-bahan organik dan inorganic yang dapat disaring dengan kertas millipore berpori pori 0,45 μm. Materi yang tersuspensi mempunyai dampak buruk terhadap kualitas air karena mengurangi penetrasi matahari ke dalam badan air, kekeruhan air meningkat yang menyebabkan gangguan pertumbuhan bagi organisme produser.')
+        st.link_button('Baca Selengkapnya', 'https://www.handalselaras.com/total-suspended-solid-tss/')
 
-    st.subheader('3. DO')
-    st.write('DO (Dissolved Oxygen) merupakan faktor penting untuk keberlangsungan hidup organisme air. Istilah Dissolved Oxygen merupakan jumlah oksigen yang tidak terikat dan terkandung bebas dalam air. Indikator ini merupakan salah satu parameter yang dapat menentukan kesehatan lingkungan air untuk organisme yang hidup didalamnya.')
-    st.link_button('Baca Selengkapnya', 'https://www.epa.gov/national-aquatic-resource-surveys/indicators-dissolved-oxygen#:~:text=What%20is%20dissolved%20oxygen%3F,of%20a%20pond%20or%20lake.')
+    with col3:
+        st.subheader('3. DO')
+        st.write('DO (Dissolved Oxygen) merupakan faktor penting untuk keberlangsungan hidup organisme air. Istilah Dissolved Oxygen merupakan jumlah oksigen yang tidak terikat dan terkandung bebas dalam air. Indikator ini merupakan salah satu parameter yang dapat menentukan kesehatan lingkungan air untuk organisme yang hidup didalamnya.')
+        st.link_button('Baca Selengkapnya', 'https://www.epa.gov/national-aquatic-resource-surveys/indicators-dissolved-oxygen#:~:text=What%20is%20dissolved%20oxygen%3F,of%20a%20pond%20or%20lake.')
 
-    st.subheader('4. BOD')
-    st.write('BOD atau Biochemical Oxygen Demand adalah suatu karakteristik yang menunjukkan jumlah oksigen terlarut yang diperlukan oleh mikroorganisme (biasanya bakteri) untuk mengurai atau mendekomposisi bahan organik dalam kondisi aerobik.')
-    st.link_button('Baca Selengkapnya', 'https://waterpedia.co.id/pengertian-cod-dan-bod/')
+    with col1:
+        st.subheader('4. BOD')
+        st.write('BOD atau Biochemical Oxygen Demand adalah suatu karakteristik yang menunjukkan jumlah oksigen terlarut yang diperlukan oleh mikroorganisme (biasanya bakteri) untuk mengurai atau mendekomposisi bahan organik dalam kondisi aerobik.')
+        st.link_button('Baca Selengkapnya', 'https://waterpedia.co.id/pengertian-cod-dan-bod/')
 
-    st.subheader('5. COD')
-    st.write('Chemical Oxygen Demand (COD) adalah ukuran jumlah bahan kimia yang dapat dioksidasi dalam air. Tingkat COD yang tinggi di Sungai Citarum dapat menunjukkan tingkat polusi organik yang tinggi, seperti yang disebabkan oleh limbah domestik dan industri. Tingkat COD yang tinggi dapat merugikan ekosistem sungai dan kesehatan manusia yang bergantung pada air.')
-    st.link_button('Baca Selengkapnya', 'https://waterpedia.co.id/pengertian-cod-dan-bod/')
+    with col2:
+        st.subheader('5. COD')
+        st.write('Chemical Oxygen Demand (COD) adalah ukuran jumlah bahan kimia yang dapat dioksidasi dalam air. Tingkat COD yang tinggi di Sungai Citarum dapat menunjukkan tingkat polusi organik yang tinggi, seperti yang disebabkan oleh limbah domestik dan industri. Tingkat COD yang tinggi dapat merugikan ekosistem sungai dan kesehatan manusia yang bergantung pada air.')
+        st.link_button('Baca Selengkapnya', 'https://waterpedia.co.id/pengertian-cod-dan-bod/')
 
-    st.subheader('6. Nitrat')
-    st.write('Nitrat adalah senyawa nitrogen yang paling teroksidasi penuh dan oleh karena itu stabil terhadap oksidasi, tetapi berpotensi menjadi agen pengoksidasi yang kuat. Nitrat yang terdapat di dalam sumber air seperti misalnya air sumur gali dan sungai umumnya berasal dari pencemaran bahan-bahan kimia (pupuk urea, ZA, dan lain-lainnya) di bagian hulu.')
-    st.link_button('Baca Selengkapnya', 'https://jurnal.unismuhpalu.ac.id/index.php/JKS/article/view/3357')
+    with col3:
+        st.subheader('6. Nitrat')
+        st.write('Nitrat adalah senyawa nitrogen yang paling teroksidasi penuh dan oleh karena itu stabil terhadap oksidasi, tetapi berpotensi menjadi agen pengoksidasi yang kuat. Nitrat yang terdapat di dalam sumber air seperti misalnya air sumur gali dan sungai umumnya berasal dari pencemaran bahan-bahan kimia (pupuk urea, ZA, dan lain-lainnya) di bagian hulu.')
+        st.link_button('Baca Selengkapnya', 'https://jurnal.unismuhpalu.ac.id/index.php/JKS/article/view/3357')
 
-    st.subheader('7. Fecal Coliform')
-    st.write('Bakteri koliform adalah kumpulan mikroorganisme yang relatif tidak berbahaya yang hidup dalam jumlah besar di usus manusia dan hewan berdarah panas dan dingin. Mereka membantu pencernaan makanan. Subkelompok spesifik dari koleksi ini adalah bakteri fecal coliform, anggota yang paling umum adalah Escherichia coli. Organisme ini dapat dipisahkan dari kelompok koliform total karena kemampuannya untuk tumbuh pada suhu tinggi dan hanya berhubungan dengan kotoran hewan berdarah panas.')
-    st.link_button('Baca Selengkapnya', 'https://www.knowyourh2o.com/outdoor-4/fecal-coliform-bacteria-in-water')
+    with col1:
+        st.subheader('7. Fecal Coliform')
+        st.write('Bakteri koliform adalah kumpulan mikroorganisme yang relatif tidak berbahaya yang hidup dalam jumlah besar di usus manusia dan hewan berdarah panas dan dingin. Mereka membantu pencernaan makanan. Subkelompok spesifik dari koleksi ini adalah bakteri fecal coliform, anggota yang paling umum adalah Escherichia coli. Organisme ini dapat dipisahkan dari kelompok koliform total karena kemampuannya untuk tumbuh pada suhu tinggi dan hanya berhubungan dengan kotoran hewan berdarah panas.')
+        st.link_button('Baca Selengkapnya', 'https://www.knowyourh2o.com/outdoor-4/fecal-coliform-bacteria-in-water')
 
-    st.subheader('8. Fosfat')
-    st.write('Fosfat adalah salah satu unsur penting bagi pembentukan protein dan metabolisme. Senyawa ini termasuk turunan fosfor yang dapat ditemukan di tanah, udara, dan sedimen. Hal tersebut dijelaskan Anthon Masela dalam buku Karaginan Rumput Laut (Eucheuma Cottonii): Tinjauan Karakteristik Fisika dan Kimia. Fosfor yang ditemukan di alam, mencakup tanah, udara, dan sedimen, memiliki bentuk senyawa fosfat (batuan fosfat). Tidak seperti senyawa bahan lain, fosfor tidak dapat ditemukan di udara bertekanan tinggi.')
-    st.link_button('Baca Selengkapnya', 'https://www.detik.com/edu/detikpedia/d-6280990/mengenal-fosfat-dan-kegunaannya-dalam-kehidupan-manusia')
+    with col2:
+        st.subheader('8. Fosfat')
+        st.write('Fosfat adalah salah satu unsur penting bagi pembentukan protein dan metabolisme. Senyawa ini termasuk turunan fosfor yang dapat ditemukan di tanah, udara, dan sedimen. Hal tersebut dijelaskan Anthon Masela dalam buku Karaginan Rumput Laut (Eucheuma Cottonii): Tinjauan Karakteristik Fisika dan Kimia. Fosfor yang ditemukan di alam, mencakup tanah, udara, dan sedimen, memiliki bentuk senyawa fosfat (batuan fosfat). Tidak seperti senyawa bahan lain, fosfor tidak dapat ditemukan di udara bertekanan tinggi.')
+        st.link_button('Baca Selengkapnya', 'https://www.detik.com/edu/detikpedia/d-6280990/mengenal-fosfat-dan-kegunaannya-dalam-kehidupan-manusia')
 
-    st.subheader('9. IP')
-    st.write('IP Rating Kode IP atau International Protection Rating, sering juga diinterpretasikan sebagai Ingress Protection Rating* terdiri dari huruf IP diikuti oleh dua digit dan huruf tambahan. didefinisikan dalam standar internasional IEC 60529, itu mengklasifikasikan derajat perlindungan yang diberikan terhadap intrusi benda padat (termasuk bagian tubuh seperti tangan dan jari), debu, kontak tidak disengaja, dan udara pada yang mengandung listrik.')
-    st.link_button('Baca Selengkapnya', 'https://multimeter-digital.com/ip-rating-tingkat-ketahanan-alat-terhadap-lingkungan.html')
+    with col3:
+        st.subheader('9. IP')
+        st.write('IP Rating Kode IP atau International Protection Rating, sering juga diinterpretasikan sebagai Ingress Protection Rating* terdiri dari huruf IP diikuti oleh dua digit dan huruf tambahan. didefinisikan dalam standar internasional IEC 60529, itu mengklasifikasikan derajat perlindungan yang diberikan terhadap intrusi benda padat (termasuk bagian tubuh seperti tangan dan jari), debu, kontak tidak disengaja, dan udara pada yang mengandung listrik.')
+        st.link_button('Baca Selengkapnya', 'https://multimeter-digital.com/ip-rating-tingkat-ketahanan-alat-terhadap-lingkungan.html')
 
     st.subheader('Team Capstone')
     with st.container():
